0516bd9a46d90b79634d98bda93d65e41abf5aaa
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-12-10 19:09:33 +0100

    first commit

---
+++ src/main/java/com/univpm/oop/WeatherPal/WeatherPalApplication.java
@@ -0,0 +1,13 @@
+package com.univpm.oop.WeatherPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherPalApplication.class, args);
+	}
+
+}
