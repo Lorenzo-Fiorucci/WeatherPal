d5d8a63af254a21912a0d7f00899f77eb65bde3c
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-12-16 09:50:45 +0100

    created InvalidFormatterException class

---
+++ src/main/java/com/univpm/oop/WeatherPal/model/Exceptions/InvalidFormatterException.java
@@ -0,0 +1,13 @@
+package com.univpm.oop.WeatherPal.model.Exceptions;
+
+public class InvalidFormatterException extends Exception {
+	
+	public InvalidFormatterException() {
+		super();
+	}
+
+	public InvalidFormatterException(String message) {
+		super(message);
+	}
+
+}
