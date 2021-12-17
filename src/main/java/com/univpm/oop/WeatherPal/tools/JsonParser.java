efc5ab0f881cf4b1c5c293a49fc0554271eb0a2c
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-12-17 16:55:47 +0100

    created tool package and JsonParser class

---
+++ src/main/java/com/univpm/oop/WeatherPal/tools/JsonParser.java
@@ -0,0 +1,26 @@
+package com.univpm.oop.WeatherPal.tools;
+
+import com.univpm.oop.WeatherPal.model.Forecast.*;
+
+import java.util.*;
+import java.security.interfaces.RSAMultiPrimePrivateCrtKey;
+import java.time.*;
+import java.time.format.*;
+
+public class JsonParser {
+	
+	public static Vector<Forecast> HourlyFile(String date, String time, byte hours, String folderPath) {
+		try {
+			LocalDate lclDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+			LocalTime lclTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+
+			Period period 
+
+		} catch(DateTimeParseException e) {
+
+		}
+
+	}
+
+
+}
