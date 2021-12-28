6972c6b6c24a5d5f8e4e278711e3f92ca0b371c0
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-12-28 13:25:30 +0100

    created StatsSerializer class

--- src/main/java/com/univpm/oop/WeatherPal/model/JsonSerializers/PopulationSerializer.java
+++ src/main/java/com/univpm/oop/WeatherPal/model/JsonSerializers/PopulationSerializer.java
@@ -22,18 +22,12 @@ public class PopulationSerializer extends StdSerializer<Population<?>> {
 	public void serialize(Population<?> pop, JsonGenerator jgen, SerializerProvider provider) throws IOException {
 		
 		jgen.writeStartObject();
-		jgen.writeFieldName(pop.getName());
-		
-		jgen.writeStartObject();
 		jgen.writePOJOField("max", pop.getMax());
 		jgen.writePOJOField("min", pop.getMin());
 		jgen.writePOJOField("average", pop.getAvg());
 		jgen.writePOJOField("variance", pop.getVar());
 		jgen.writePOJOField("standard deviation", pop.getStdDev());
 		jgen.writeEndObject();
-		
-		jgen.writeEndObject();
-		
 	}
 
 }
---
+++ src/main/java/com/univpm/oop/WeatherPal/model/JsonSerializers/StatsSerializer.java
@@ -0,0 +1,35 @@
+package com.univpm.oop.WeatherPal.model.JsonSerializers;
+
+import com.univpm.oop.WeatherPal.model.Statistics.Stats;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+public class StatsSerializer extends StdSerializer<Stats> {
+
+	public StatsSerializer() {
+		this(null);
+	}
+
+	public StatsSerializer(Class<Stats> klazz) {
+		super(klazz);
+	}
+
+	@Override
+	public void serialize(Stats stat, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+		
+		jgen.writeStartObject();
+		jgen.writePOJOField(stat.getTemp().getName(), stat.getTemp());
+		jgen.writePOJOField(stat.getFeelsLike().getName(), stat.getFeelsLike());
+		jgen.writePOJOField(stat.getHumidity().getName(), stat.getHumidity());
+		jgen.writePOJOField(stat.getWind().getName(), stat.getWind());
+		jgen.writePOJOField(stat.getPressure().getName(), stat.getPressure());
+		jgen.writePOJOField(stat.getClouds().getName(), stat.getClouds());
+		jgen.writePOJOField(stat.getAirPoll().getName(), stat.getAirPoll());
+		jgen.writePOJOField("period", stat.getPeriod());
+		jgen.writeEndObject();
+	}
+}
