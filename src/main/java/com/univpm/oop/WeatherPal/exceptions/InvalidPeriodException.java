2d2a8a00aade4dc06a2c431210dd6bc9814432ff
Lorenzo Fiorucci <S1099692@studenti.univpm.it>
2021-12-29 22:24:35 +0100

    Add InvalidPeriodException

---
+++ src/main/java/com/univpm/oop/WeatherPal/exceptions/InvalidPeriodException.java
@@ -0,0 +1,12 @@
+package com.univpm.oop.WeatherPal.exceptions;
+
+public class InvalidPeriodException extends Exception {
+
+    public InvalidPeriodException() {
+        super();
+    }
+
+    public InvalidPeriodException(String message) {
+        super(message);
+    }
+}
