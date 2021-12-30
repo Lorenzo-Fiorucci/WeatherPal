3233d814b7d0a99d126a14c6ef5820252a1ec8dd
Lorenzo Fiorucci <S1099692@studenti.univpm.it>
2021-12-30 11:14:48 +0100

    Update StatsService and StatServiceImpl

--- src/main/java/com/univpm/oop/WeatherPal/service/StatsServiceImpl.java
+++ src/main/java/com/univpm/oop/WeatherPal/service/StatsServiceImpl.java
@@ -1,12 +1,10 @@
 package com.univpm.oop.WeatherPal.service;
 
-import com.univpm.oop.WeatherPal.model.Filters.DailyPeriod;
 import com.univpm.oop.WeatherPal.model.Filters.HourlyPeriod;
 import com.univpm.oop.WeatherPal.model.Measures.Measure;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -19,7 +17,7 @@ public class StatsServiceImpl implements StatsService {
     }
 
     @Override
-    public DailyPeriod getPeriod(String folderPath) {
+    public HourlyPeriod getHouPer(String folderPath) {
 
         String root = System.getProperty("user.dir");
         File folder = new File(root + folderPath);
@@ -28,19 +26,13 @@ public class StatsServiceImpl implements StatsService {
         String from = fileNames[0].substring(0, fileNames[0].length() - 4);
         String to = fileNames[fileNames.length - 1].substring(0, fileNames[fileNames.length - 1].length() - 4);
 
-        DailyPeriod period = null;
+        HourlyPeriod period = null;
 
         if (folderPath.equals("\\src\\main\\resources\\static\\Every1h")) {
             LocalDateTime from1 = LocalDateTime.parse(from, DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm"));
             LocalDateTime to1 = LocalDateTime.parse(to, DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm"));
 
             period = new HourlyPeriod(from1, to1);
-        }
-        if (folderPath.equals("\\src\\main\\resources\\static\\Weekly")) {
-            LocalDate from2 = LocalDate.parse(from, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-            LocalDate to2 = LocalDate.parse(to, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-
-            period = new DailyPeriod(from2, to2);
         }
 
         return period;
